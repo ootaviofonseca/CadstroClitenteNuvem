114cef74de355b78b78488b55004fa282bd060dc
Otavio Fonseca <otavioaf123@gmail.com>
2024-08-24 12:26:01 -0300

    Update db.js

--- api/db.js
+++ api/db.js
@@ -9,7 +9,7 @@ export const db = mysql.createConnection({
 import mysql from "mysql"
 
 export const db = mysql.createConnection({
-    host: 'http://3.238.158.21',
+    host: '3.238.158.21',
     port: "3306",
     user: 'mysuperuser',
     password: 'mysuperuser',
